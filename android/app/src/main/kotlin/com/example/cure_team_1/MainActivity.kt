c927d7669e158147eccecacc85c711d07f4e51bc
Hazem Mohammed <hazemhkcyy@gmail.com>
2025-12-07 00:32:28 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/cure_team_1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cure_team_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+class AppColors {
+  /// Primary text color (Hint: Also used for some widgets like Buttons, Labels, etc.).
+  static const Color text = Color(0xFF05162C);
+}

0d5b5c954a8743b37776c51c07e7aac44c7548ef
Mohamedyasser1100 <mohamedyasser9933@gmail.com>
2025-12-08 15:31:30 +0200

    feat: add splash screen & assets folder, restructure auth feature

--- lib/app.dart
+++ lib/app.dart
@@ -1,18 +1,21 @@
+import 'package:cure_team_1/features/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Cure extends StatelessWidget {
+  const Cure({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'cure_team_1',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        useMaterial3: true,
+    return const ScreenUtilInit(
+      designSize: Size(390, 812),
+      minTextAdapt: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: SplashScreen(),
+        ),
       ),
-      home: Container(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -3,4 +3,5 @@ import 'dart:ui';
 class AppColors {
   /// Primary text color (Hint: Also used for some widgets like Buttons, Labels, etc.).
   static const Color text = Color(0xFF05162C);
+  static const Color primaryColor = Color(0xff145db8);
 }

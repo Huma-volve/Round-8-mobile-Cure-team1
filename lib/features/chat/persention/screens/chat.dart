e945adcec17b279351f3ab1c5d537e1e73f2a1b3
mafdysaad <mafdysaad366@gmail.com>
2025-12-08 23:50:07 +0200

    rsolve conflict

--- lib/features/chat/persention/screens/chat.dart
+++ lib/features/chat/persention/screens/chat.dart
@@ -1,9 +1,5 @@
-import 'package:cure_team_1/core/theme/app_text_styles.dart';
-import 'package:cure_team_1/core/utils/assets.dart';
 import 'package:cure_team_1/features/chat/persention/screens/widget/customabppar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/svg.dart';
 
 class Chat extends StatefulWidget {
   const Chat({super.key});

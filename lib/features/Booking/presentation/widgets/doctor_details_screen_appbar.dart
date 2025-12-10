4969296d811bf5f0cbcea870a5121d23068a0708
Mohamed Shaban <pmohamedshabaan@gmail.com>
2025-12-10 13:43:22 +0200

    update book appbar

--- lib/features/Booking/presentation/widgets/doctor_details_screen_appbar.dart
+++ lib/features/Booking/presentation/widgets/doctor_details_screen_appbar.dart
@@ -1,7 +1,9 @@
+
 import 'package:cure_team_1/core/style/theme/app_text_styles.dart';
 import 'package:cure_team_1/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
 
 class BookingAppBar extends StatelessWidget {
   const BookingAppBar({
@@ -13,11 +15,16 @@ final bool ismessageIconEnable;
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Container(
-          height: 24.h,
-          width: 24.w,
-          decoration: const BoxDecoration(
-              image: DecorationImage(image: AssetImage(Assets.resourceImagesArrowImage))),
+        GestureDetector(
+          onTap: () {
+          GoRouter.of(context).pop();
+          },
+          child: Container(
+            height: 24.h,
+            width: 24.w,
+            decoration: const BoxDecoration(
+                image: DecorationImage(image: AssetImage(Assets.resourceImagesArrowImage))),
+          ),
         ),
         const Spacer(),
         Text(

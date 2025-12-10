90326ed0e57a6602b26525bac3c4422e34a89275
Mohamed Shaban <pmohamedshabaan@gmail.com>
2025-12-10 13:44:58 +0200

    finish book appointment screen

--- lib/features/Booking/presentation/screens/book_apointmennt_.dart
+++ lib/features/Booking/presentation/screens/book_apointmennt_.dart
@@ -1,10 +1,7 @@
 
-import 'package:cure_team_1/core/style/theme/app_text_styles.dart';
-import 'package:cure_team_1/features/Booking/presentation/widgets/calender.dart';
-import 'package:cure_team_1/features/Booking/presentation/widgets/doctor_details_screen_appbar.dart';
-import 'package:cure_team_1/features/Booking/presentation/widgets/doctor_details_widget.dart';
+import 'package:cure_team_1/features/Booking/presentation/widgets/book_apointment_body.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 
 
 class Bookappointment extends StatelessWidget {
@@ -12,25 +9,9 @@ class Bookappointment extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding:  EdgeInsets.symmetric(horizontal: 16.w),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 50.h,),
-              const BookingAppBar(title: 'Book Appointment',ismessageIconEnable: false,),
-               SizedBox(height: 35.h,),
-              const DoctorDetailsWidget(),
-               SizedBox(height: 24.h,),
-              Text('Select a day',style: AppTextStyles.georgiaRegular20,),
-              SizedBox(height: 16.h,),
-         const  Expanded(child:    SingleMonthCalendar()),
-         
-        
-          ],
-        ),
-      ),
+    return const Scaffold(
+      body: BookApointmentBody(),
     );
   }
 }
+

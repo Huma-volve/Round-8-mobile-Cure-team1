417e10a7de2e30925f30ed72b69c63679ad590a9
Mohamed Shaban <pmohamedshabaan@gmail.com>
2025-12-10 17:37:41 +0200

    create paymetn method items and switch between active and inactive items

---
+++ lib/features/Booking/data/models/payment_method_model.dart
@@ -0,0 +1,15 @@
+import 'package:cure_team_1/core/utils/assets.dart';
+
+class PaymentMethodModel {
+  final String title;
+  final String image;
+
+  PaymentMethodModel({required this.title, required this.image});
+}
+List<PaymentMethodModel>paymentItemsList=[
+  PaymentMethodModel(title: 'Credit Cart', image: Assets.resourceImagesVisa),
+ PaymentMethodModel(title: 'PayPal', image: Assets.resourceImagesPaypal),
+
+ PaymentMethodModel(title: 'Apple Pay', image: Assets.resourceImagesApplePay),
+
+];
